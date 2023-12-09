96f8ef134eb10de26a50a7c2b5be5b4fc0cba294
Douglas Poma <douglaspoma@yahoo.com>
2023-12-09 11:40:01 -0300

    feat: server runnin with api version endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
+use rust_actix::infra::endpoints::config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(|| async { return "Hello World!" })))
+    HttpServer::new(|| App::new().service(web::scope("/api").configure(config)))
         .bind(("127.0.0.1", 4000))?
         .run()
         .await
